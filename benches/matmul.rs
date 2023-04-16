5bc19e03bf45fa60c81c9b6e26d20d2bba582dec
Parsa Ghadimi <i@parsa.ooo>
2023-04-16 19:31:56 -0400

    matmul different sizes

--- benches/matmul.rs
+++ benches/matmul.rs
@@ -7,15 +7,17 @@ fn bench_matmul(c: &mut Criterion) {
     let mut g = c.benchmark_group("matmul");
     g.sample_size(20);
 
-    g.bench_function("ndarray", |b| {
-        let v1 = Array::random((10_000, 10_000), Uniform::new(0., 10.));
-        let v2 = Array::random((10_000, 10_000), Uniform::new(0., 10.));
+    for size in [10, 100, 500, 1000] {
+        g.bench_with_input(BenchmarkId::new("ndarray", size), &size, |b, size| {
+            let v1 = Array::random((*size, *size), Uniform::new(0., 10.));
+            let v2 = Array::random((*size, *size), Uniform::new(0., 10.));
 
-        b.iter(|| {
-            let r = &v1 * &v2;
-            black_box(r);
-        })
-    });
+            b.iter(|| {
+                let r = &v1 * &v2;
+                black_box(r);
+            })
+        });
+    }
 }
 
 criterion_group!(benches, bench_matmul);
